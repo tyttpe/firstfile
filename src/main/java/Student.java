a19fdf9e5b60f386f88692eadffc9bd691c4036a
tyttpe <994148249@qq.com>
2019-02-28 18:47:55 +0800

    this is my first file

---
+++ src/main/java/Student.java
@@ -0,0 +1,10 @@
+package main.java;
+
+/**
+ * Created by DELL on 2019/2/28.
+ */
+public class Student {
+    public static void main(String[] args) {
+        System.out.println("helloworld");
+    }
+}
